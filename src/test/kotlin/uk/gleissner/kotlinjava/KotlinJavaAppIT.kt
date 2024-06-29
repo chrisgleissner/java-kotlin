aee1591cea0f5b378e5049d40ed4ddc13aa87f58
Christian Gleissner <chrisgleissner@gmail.com>
2024-06-29 15:53:46 +0100

    Increase coverage

---
+++ src/test/kotlin/uk/gleissner/kotlinjava/KotlinJavaAppIT.kt
@@ -0,0 +1,12 @@
+package uk.gleissner.kotlinjava
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+internal class KotlinJavaAppIT {
+
+    @Test
+    fun `can wire`() {
+    }
+}
