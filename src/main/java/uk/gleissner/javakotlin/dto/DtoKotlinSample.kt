9eff922568167bf1e0189649b468160f69b7fd94
Christian Gleissner <chrisgleissner@gmail.com>
2024-06-29 16:29:12 +0100

    Aligned code order

--- src/main/java/uk/gleissner/javakotlin/dto/DtoKotlinSample.kt
+++ src/main/java/uk/gleissner/javakotlin/dto/DtoKotlinSample.kt
@@ -5,14 +5,14 @@ import uk.gleissner.javakotlin.CodeSample
 
 class DtoKotlinSample : CodeSample {
 
-    data class Department(val name: String, val head: Employee?)
-
-    data class Employee(val name: String)
-
     private val log = KotlinLogging.logger {}
 
     override fun run(args: List<String>) {
         val department = Department(name = args[0], head = Employee(name = "John Doe"))
         log.info { "The head of ${department.name} department is ${department.head?.name ?: "Unknown"}" }
     }
+
+    data class Department(val name: String, val head: Employee?)
+
+    data class Employee(val name: String)
 }
