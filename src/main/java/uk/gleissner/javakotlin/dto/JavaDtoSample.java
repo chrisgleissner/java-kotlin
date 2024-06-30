ec46d1f1cf2566fa8c223345514a8e4a65502eca
Christian Gleissner <chrisgleissner@gmail.com>
2024-06-30 13:36:48 +0100

    Removed @SneakyThrows since Lombok does not yet fully support Java 22

--- src/main/java/uk/gleissner/javakotlin/dto/JavaDtoSample.java
+++ src/main/java/uk/gleissner/javakotlin/dto/JavaDtoSample.java
@@ -1,10 +1,10 @@
 package uk.gleissner.javakotlin.dto;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Builder;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.jetbrains.annotations.NotNull;
@@ -19,7 +19,6 @@ public class JavaDtoSample implements DtoSample {
     private final ObjectMapper objectMapper;
 
     @NotNull
-    @SneakyThrows
     @Override
     public String departmentJson(@NonNull String departmentName, String departmentHeadName) {
         val department = Department.builder()
@@ -28,12 +27,15 @@ public class JavaDtoSample implements DtoSample {
                 .map(n -> Employee.builder().name(n).build())
                 .orElse(null))
             .build();
-        val departmentJson = objectMapper.writeValueAsString(department);
-        log.info("Created department JSON for (departmentName={}, employeeName={}): {}", departmentName, departmentHeadName, departmentJson);
-        return departmentJson;
+        try {
+            val departmentJson = objectMapper.writeValueAsString(department);
+            log.info("Created department JSON for (departmentName={}, employeeName={}): {}", departmentName, departmentHeadName, departmentJson);
+            return departmentJson;
+        } catch (JsonProcessingException e) { // Required since Java 22 does not support @SneakyThrows as of Lombok v1.18.34
+            throw new RuntimeException(e);
+        }
     }
-
-
+    
     @Builder
     record Department(@NonNull String name, Employee head) {
     }
