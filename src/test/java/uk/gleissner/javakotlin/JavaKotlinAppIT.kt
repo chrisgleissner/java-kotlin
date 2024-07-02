278a2630061c71af03f0cc5bb91b4ec45f2b357e
Christian Gleissner <chrisgleissner@gmail.com>
2024-07-02 17:26:45 +0100

    Added IT for main app entrance point

---
+++ src/test/java/uk/gleissner/javakotlin/JavaKotlinAppIT.kt
@@ -0,0 +1,11 @@
+package uk.gleissner.javakotlin
+
+import org.junit.jupiter.api.Test
+
+internal class JavaKotlinAppIT {
+
+    @Test
+    fun `can wire`() {
+        JavaKotlinApp.main(arrayOf())
+    }
+}
